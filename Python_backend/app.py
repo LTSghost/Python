d987c37e22a78c63aff9a551dee355cea8e60766
田匡宇 <DKLTSghost@gmail.com>
2021-09-28 00:30:26 +0800

    新增一些網頁與引入更多物件跟函式

--- Python_backend/app.py
+++ Python_backend/app.py
@@ -1,18 +1,46 @@
+from os import name
 from flask import Flask #引入Flask 
 from flask import request #引入request 物件
-
+from flask import redirect #引入redirect 函式
+from flask import render_template #引入render_temple 函式
+import json #引入json模組
 #建立Application物件......可以額外建立靜態檔案的路徑
 app = Flask(
     __name__,
     static_folder = "public", #靜態檔案對應的資料夾
-    static_url_path= "/abc" #靜態檔案對應的網址路徑
+    static_url_path= "/abc"   #靜態檔案對應的網址路徑
 )
-
 #所有在 static 資料夾底下的檔案, 都對應道網址路徑。/static/檔案名稱
 
+@app.route('/page2')
+def templates_index2():
+    return render_template('page2.html')
+
+@app.route('/templates0')
+def templates_index0():
+    return render_template('index', name = "LTS")
+
+@app.route('/templates1')
+def templates_index1():
+    return render_template('page1.html')
+
+@app.route('/en/')
+def index_en():
+    return json.dumps({     #字典轉換json格式字串
+            "text":"Home Page", #回傳路徑 / 的內容
+            "status":"OK"
+        })
+
+@app.route('/zh/')
+def index_zh():
+    return json.dumps({
+            "text":"首頁",
+            "status":"OK"
+        }, ensure_ascii = False) #指示不要用ASCII碼編譯成中文
 
 @app.route("/") #建立路徑 / (根目錄)回應方式
 def get_home(): #用來回應路徑 / 的處理函式
+    # return redirect('https:/www.google.com/')  #導向到指定網址
     print("請求方法", request.method) #print在伺服器端
     print("通訊協定", request.scheme)
     print("主機名稱", request.host)
@@ -23,9 +51,11 @@ def get_home(): #用來回應路徑 / 的處理函式
     print("使用者IP", request.remote_addr)
     lang = request.headers.get("accept-language")
     if lang.startswith('en'):
-        return "Home Page" #回傳路徑 / 的內容
+        return redirect('/en/')
+        
     else:
-        return "首頁"
+        return redirect('/zh/')
+
 
 @app.route("/data")
 def get_Data():
@@ -38,5 +68,17 @@ def get_Name(name): #設定參數
     else:
         return "Hey " + name + ", Where is Howard?"
 
+#利用要求字串(Query String) 提供彈性
+@app.route("/getSum")
+def getSum():
+    minNum = request.args.get("min",1)
+    minNum = int(minNum)
+    maxNum = request.args.get("max",100)
+    maxNum = int(maxNum)
+    result = 0
+    for i in range(minNum,maxNum+1):
+        result += i
+    print(result)
+    return str(result)
 
 app.run(port = 3000) #啟動網站伺服器 , 可透過 port 參數指定埠號
